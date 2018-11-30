2c6c9ac345ec6b2d2cebcc89ee96244e67c3c59a
alexdenisova <37692145+alexdenisova@users.noreply.github.com>
2018-11-30 22:06:44 +0700

    super old crypt.c

---
+++ prog.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <crypt.h>
+#include <setjmp.h>
+
+jmp_buf  env;
+
+void checkPassword()
+{
+    char *password = NULL;
+    char *hash = NULL;
+   
+    scanf("%ms",&password);
+    scanf("%ms",&hash); 
+    
+    //crypt password
+    char *cryptPassword = crypt(password, "00");
+    if(cryptPassword != NULL) {
+        if(strcmp(cryptPassword, hash) == 0) 
+        {
+            printf("allow\n");
+        } else {
+            printf("deny\n");
+        }
+        free(password);
+        free(hash);
+    } else {
+        longjmp (env, 42);
+    }
+}
+
+int main()
+{
+    if(setjmp (env)) {
+       printf("Error");
+    } else {
+       checkPassword();
+    }
+   
+    return 256;
+}
