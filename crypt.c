061dbfd0d491f075be99c1f9ad147c0d8a579718
alexdenisova <37692145+alexdenisova@users.noreply.github.com>
2018-11-30 22:00:28 +0700

    help

--- crypt.c
+++ crypt.c
@@ -1,34 +1,28 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <crypt.h>
-#include <setjmp.h>
 
-
-void checkPassword() {
-    char *password = NULL;
-    char *hash = NULL;
-
-    scanf("%ms",&password);  
-    scanf("%ms",&hash); 
+int main()
+{
+  char *password = NULL;
+  char *hash = NULL;
     
-    //crypt password
-    char *cryptPassword = crypt(password, "4r");
-    if(cryptPassword != NULL) {
-        if(strcmp(cryptPassword, hash) == 0) {
-            printf("allow\n");
-        } else {
-            printf("deny\n");
-        }
-        free(password);
-        free(hash);
-    } else {
-      printf("Error\n");
-    }
-}
+  scanf("%ms",&password);
+  scanf("%ms",&hash);
 
-int main(){
-    checkPassword();
-    return 0;
+  char *cryptPassword = crypt(password, "00");
+  if(cryptPassword == NULL){
+    printf("Error\n");
+    }else{
+      if(strcmp(cryptPassword, hash) == 0) {
+        printf("allow\n");
+      } else {
+        printf("deny\n");
+    }
+  }
+  free(password);
+  free(hash);
+  return 0;
 }
